345ae7cfb03531bc00871861df969bfe52877aeb
RyzhakovAV <rjizhakov@mail.ru>
2023-03-10 19:17:22 +0500

    modified Cycles task table multiplication

--- CyclesTheme.java
+++ CyclesTheme.java
@@ -204,6 +204,22 @@ public class CyclesTheme{
 
         System.out.println("Task 10 Вывод таблицы умножения Пифагора");
 
+        for(int i = 1; i < 10; i++) {
+            for(int j = 1; j < 10; j++) {
+                if(i == 1 && j == 1) {
+                    System.out.print("      |");
+                }else if(j == 1) {
+                    System.out.printf("%4d  |", i * j);
+                }else{
+                    System.out.printf("%4d", i * j);
+                }
+            }
+            System.out.println();
+            if(i == 1) {
+                System.out.println("----------------------------------------");
+            }
+        }
+
         System.out.println("\n-------------------------------\n");
     }
 }
