e025885237b729c48304f019324a85b47a07555b
RyzhakovAV <rjizhakov@mail.ru>
2023-04-29 14:17:26 +0500

    new task my firt game, need find number

---
+++ Lesson_1/MyFirstGame.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class MyFirstGame {
+	public static void main(String[] args) {
+	int randomNumber = (int)(Math.random() * 100);	
+	Scanner console = new Scanner(System.in);
+	System.out.println("Компьютер загадал число. Угадайте число:");
+	String inputNumber = console.nextLine();
+	Boolean condition = true;
+	
+	while(condition) {
+		try {
+			int number = Integer.parseInt(inputNumber);
+			if(number < randomNumber) {
+				System.out.println("Ваше число меньше загаданного. Введите другое число: ");
+			}else if(number > randomNumber) {
+				System.out.println("Ваше число больше загаданного. Введите другое число: ");
+			}else{
+				System.out.println("Вы угадали, это было число: " + randomNumber);
+				break;
+			}
+			inputNumber = console.nextLine();
+		} catch(Exception e) {
+			System.out.println("Ошибка ввода");
+		}
+	}
+	}
+}
