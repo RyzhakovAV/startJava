371a336e8cd0ef65ce29c38f287a36f7ad700157
RyzhakovAV <rjizhakov@mail.ru>
2023-05-05 16:06:47 +0500

    study OOP class and object

---
+++ Lesson_2/Wolf.java
@@ -0,0 +1,28 @@
+public class Wolf {
+	char gender;
+	String nickname;
+	float weight;
+	int age;
+	String color;
+	
+	
+	void move() {
+		System.out.println("Идет");
+	}
+	
+	void sit() {
+		System.out.println("Сидит");
+	}
+	
+	void run() {
+		System.out.println("Бежит");
+	}
+	
+	void howl() {
+		System.out.println("Воет");
+	}
+	
+	void hunter() {
+		System.out.println("Охотится");
+	}
+}
