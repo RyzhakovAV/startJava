d82837598afd58d70af41c14b4f704b42e57de65
RyzhakovAV <rjizhakov@mail.ru>
2023-06-09 10:59:29 +0500

    work with array. task 2 about multiplication of numbers in array

--- src/com/startjava/lesson_4/ArrayTheme.java
+++ src/com/startjava/lesson_4/ArrayTheme.java
@@ -8,6 +8,29 @@ public class ArrayTheme {
         System.out.println();
     }
 
+    public static void multiArray() {
+        System.out.println("2. Вывод произведения элементов массива");
+        int[] multiArr = fillArray(10);
+        printArray(multiArr);
+        int R = 1;
+        for(int i = 0; i < multiArr.length; i++) {
+            if(multiArr[i] != 0 && multiArr[i] != 9) {
+                R *= multiArr[i];
+            }
+        }
+        System.out.println();
+        System.out.println(R > 1 ? "Произведение числе в массиве равно: " + R : " 1");
+    }
+
+    public static int[] fillArray(int size) {
+        int[] array = new int[size];
+
+        for(int i = 0; i < array.length; i++) {
+            array[i] = i;
+        }
+        return array;
+    }
+
     public static void reverse(int[] array) {
         int[] copyArray = new int[array.length];
         for(int i = 0; i < array.length; i ++) {
@@ -35,6 +58,7 @@ public class ArrayTheme {
     public static void main(String[] args) {
         int[] arrayNumbers = {1, 5, 3, 2, 6, 7, 4};
         reverseArray(arrayNumbers);
-
+        System.out.println();
+        multiArray();
     }
 }
